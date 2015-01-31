337f75aea8c68ea110eb5b1395f37841d1b8e833
Joshua Manricks <josh@thesecretlocation.com>
2015-01-31 13:47:40 -0500

    Cave generation

---
+++ Assets/Example/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private MazeCell currentCell;
+
+	private MazeDirection currentDirection;
+
+	public void SetLocation (MazeCell cell) {
+		if (currentCell != null) {
+			currentCell.OnPlayerExited();
+		}
+		currentCell = cell;
+		Vector3 pos = cell.transform.localPosition;
+		pos.y = 2f;
+		transform.localPosition = pos;
+//		transform.localPosition.Set (transform.localPosition.x, 10f, transform.localPosition.z);
+		currentCell.OnPlayerEntered();
+
+		Debug.Log (transform.localPosition);
+
+	}
+
+//	private void Move (MazeDirection direction) {
+//		MazeCellEdge edge = currentCell.GetEdge(direction);
+//		if (edge is MazePassage) {
+//			SetLocation(edge.otherCell);
+//		}
+//	}
+//
+//	private void Look (MazeDirection direction) {
+//		transform.localRotation = direction.ToRotation();
+//		currentDirection = direction;
+//	}
+
+//	private void Update () {
+//		if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+//			Move(currentDirection);
+//		}
+//		else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+//			Move(currentDirection.GetNextClockwise());
+//		}
+//		else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+//			Move(currentDirection.GetOpposite());
+//		}
+//		else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+//			Move(currentDirection.GetNextCounterclockwise());
+//		}
+//		else if (Input.GetKeyDown(KeyCode.Q)) {
+//			Look(currentDirection.GetNextCounterclockwise());
+//		}
+//		else if (Input.GetKeyDown(KeyCode.E)) {
+//			Look(currentDirection.GetNextClockwise());
+//		}
+//	}
+}
---
+++ Assets/caves/CaveGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaveGenerator : MonoBehaviour {
+
+	public GameObject[] TILES;
+
+//	int 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+}
